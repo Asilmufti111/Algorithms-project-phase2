704cb448109b4db53333cc335ecfa7b458678ec5
qamaralashrafi <122994637+qamaralashrafi@users.noreply.github.com>
2023-05-28 19:46:56 +0300

    Update Edge.java

--- Algorithms project phase2/src/GraphFramework/Edge.java
+++ Algorithms project phase2/src/GraphFramework/Edge.java
@@ -1,13 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GraphFramework;
 
-/**
- *
- * @author asil
+
+/*
+ *  @authors Asil, Qamar, Aroub,Khalida
+ * B9A
+ * CPCS-324
+ * Project Code
+ * 4th June 2023
  */
-public class Edge {
-    
+public class Edge implements Comparable<Edge> {
+
+    private int weight;
+    private Vertex source;
+    private Vertex target;
+    private Vertex parent;
+
+
+
+    public Edge(Vertex source, Vertex target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Vertex getSource() {
+        return source;
+    }
+
+    public void setSource(Vertex source) {
+        this.source = source;
+    }
+
+    public Vertex getTarget() {
+        return target;
+    }
+
+    public void setTarget(Vertex target) {
+        this.target = target;
+    }
+
+    public Vertex getParent() {
+        return parent;
+    }
+
+    public void setParent(Vertex parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public int compareTo(Edge e) { // compare based on weights
+        if (this.weight > e.weight) {
+            return 1;
+        } else if (this.weight == e.weight) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public int parent(int pos) { // return the parent of the edge's node in the heap       
+       
+        return pos / 2;
+    }
+
+    public void displayInfo() {
+        System.out.print("Office No. " + source.getLabel());
+        System.out.print(" - ");
+        System.out.print("Office No. " + target.getLabel());
+        System.out.print(" : line length: " + weight + " ");
+    }
+
 }
+

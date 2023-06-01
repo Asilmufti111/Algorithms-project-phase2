dc6468bd0ab3ea7b147c9a5697158d984e72ae54
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-06-01 10:54:29 +0300

    Update AFRouteMap.java

--- Algorithms project phase2/src/AirFreightApp/AFRouteMap.java
+++ Algorithms project phase2/src/AirFreightApp/AFRouteMap.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AirFreightApp;
 
-/**
- *
- * @author asil
+import GraphFramework.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+/*
+ *  @authors Asil, Qamar, Aroub,Khalida
+ * B9A
+ * CPCS-324
+ * Project Code
+ * 18th may. 2023
  */
-public class AFRouteMap {
-    
+public class AFRouteMap extends Graph {
+
+    AFRouteMap(int verticesNo, int edgesNo, boolean b) {
+        super(verticesNo, edgesNo, b);
+    }
+
+    AFRouteMap(File graphfile) throws FileNotFoundException {
+
+        super(graphfile);
+    }
+
+    AFRouteMap() {
+
+    }
+
+    // Create Vertex
+    @Override
+    public Vertex createVertex(String label) {
+        return new Location(label);
+    }
+
+    // Create Edge
+    @Override
+    public Edge createEdge(Vertex source, Vertex target, int weight) {
+        return new Route(source, target, weight);
+    }
+
+    public void displayInfo() {
+        displayInfo();
+    }
+
 }

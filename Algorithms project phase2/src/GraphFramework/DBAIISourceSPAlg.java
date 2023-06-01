a73cebd19a91bd2664f8c76b6e4be1e8fc17b3c7
qamaralashrafi <122994637+qamaralashrafi@users.noreply.github.com>
2023-06-01 15:37:44 +0300

    Update DBAIISourceSPAlg.java

--- Algorithms project phase2/src/GraphFramework/DBAIISourceSPAlg.java
+++ Algorithms project phase2/src/GraphFramework/DBAIISourceSPAlg.java
@@ -9,5 +9,24 @@ package GraphFramework;
  * @author asil
  */
 public class DBAIISourceSPAlg {
+    Graph graph;
     
+    SingleSourceSPAlg SPAlg;
+    
+    public DBAIISourceSPAlg(Graph graph){
+                this.graph= graph;
+    }
+    
+    public void computeDijkstraBasedSPAlg(){
+    
+        for(int i=0;i<graph.verticesNo;i++){
+    
+        System.out.println("The starting point location is "+graph.vertices.get(i));
+    
+        System.out.println("The routes from location "+graph.vertices.get(i)+" to the rest of the locations are:");
+    
+    
+        SPAlg.computeDijkstraAlg(graph,graph.vertices.get(i));
+    }}
 }
+

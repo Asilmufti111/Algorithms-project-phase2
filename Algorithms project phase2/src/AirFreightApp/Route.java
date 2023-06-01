e2f1fcaffd7516ab6557794374bb5dd14de7039b
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-06-01 10:53:31 +0300

    Update Route.java

--- Algorithms project phase2/src/AirFreightApp/Route.java
+++ Algorithms project phase2/src/AirFreightApp/Route.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AirFreightApp;
 
-/**
- *
- * @author asil
+import GraphFramework.*;
+
+/*
+ *  @authors Asil, Qamar, Aroub,Khalida
+ * B9A
+ * CPCS-324
+ * Project Code
+ * 18th may. 2023
  */
-public class Route {
-    
-}
+public class Route extends Edge {
+
+    public Route(Vertex source, Vertex target, int weight) {
+        super(source, target, weight);
+    }
+
+    // Methods
+    @Override
+    public void displayInfo() {
+        System.out.println("---route length: " + super.getWeight());
+    } // End of Method
+
+} // End of Class

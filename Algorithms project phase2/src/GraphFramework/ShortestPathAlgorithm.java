db492e0d1f306c20756b90c942ff307362aa2817
qamaralashrafi <122994637+qamaralashrafi@users.noreply.github.com>
2023-06-03 01:21:11 +0300

    Update ShortestPathAlgorithm.java

--- Algorithms project phase2/src/GraphFramework/ShortestPathAlgorithm.java
+++ Algorithms project phase2/src/GraphFramework/ShortestPathAlgorithm.java
@@ -4,10 +4,21 @@
  */
 package GraphFramework;
 
-/**
- *
- * @author asil
+/*
+ *  @authors Asil, Qamar, Aroub,Khalida
+ * B9A
+ * CPCS-324
+ * Project Code
+ * 4th June. 2023
  */
 public class ShortestPathAlgorithm {
+    Graph graph;
+    DBAIISourceSPAlg allSources;
     
+    public ShortestPathAlgorithm(Graph graph){
+        
+    this.graph=graph;
+    
+    allSources=new DBAIISourceSPAlg(graph);
+    }
 }

13c737b356d6a8a4b9e3515f4809696caec6f984
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-06-03 15:19:17 +0300

    Update DBAIISourceSPAlg.java

--- Algorithms project phase2/src/GraphFramework/DBAIISourceSPAlg.java
+++ Algorithms project phase2/src/GraphFramework/DBAIISourceSPAlg.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GraphFramework;
 
 /*
@@ -12,26 +8,28 @@ package GraphFramework;
  * 4th June. 2023
  */
 public class DBAIISourceSPAlg {
+
     Graph graph;
-    
+
     SingleSourceSPAlg SPAlg;
-    
-    public DBAIISourceSPAlg(Graph graph){
-        this.graph= graph;
-        
+
+    public DBAIISourceSPAlg(Graph graph) {
+        this.graph = graph;
+
         computeDijkstraBasedSPAlg();
     }
-    
-    public void computeDijkstraBasedSPAlg(){
-    
-        for(int i=0;i<graph.verticesNo;i++){//go thro all vertices
-            
-            SPAlg= new SingleSourceSPAlg();
-    
-            System.out.println("\nThe starting point location is "+graph.vertices.get(i).getLabel()); //get source
-            
-            System.out.println("\nThe routes from location "+graph.vertices.get(i).getLabel()+" to the rest of the locations are:");
-            
-            SPAlg.computeDijkstraAlg(graph,graph.vertices.get(i));//compute dijkstra
-    }}
+
+    public void computeDijkstraBasedSPAlg() {
+
+        for (int i = 0; i < graph.verticesNo; i++) {//go thro all vertices
+
+            SPAlg = new SingleSourceSPAlg();
+
+            System.out.println("\nThe starting point location is " + (char) (Integer.parseInt(graph.vertices.get(i).getLabel()) + 'A')); //get source
+
+            System.out.println("\nThe routes from location " + (char) (Integer.parseInt(graph.vertices.get(i).getLabel()) + 'A') + " to the rest of the locations are:");
+
+            SPAlg.computeDijkstraAlg(graph, graph.vertices.get(i));//compute dijkstra
+        }
+    }
 }
